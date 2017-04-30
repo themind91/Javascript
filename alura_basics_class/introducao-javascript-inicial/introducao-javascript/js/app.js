e8a588c13ae3c6f976be044e6ba3108a293b4faa
Victor Rocha <themind1991@gmail.com>
2017-04-30 19:06:28 -0300

    factoring the code creating functions

--- alura_basics_class/introducao-javascript-inicial/introducao-javascript/js/app.js
+++ alura_basics_class/introducao-javascript-inicial/introducao-javascript/js/app.js
@@ -1,12 +1,36 @@
-console.log("Hello world");
-var name = "Victor Rocha";
-document.querySelector('.titulo').textContent = name;
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var tdAltura = paciente.querySelector(".info-altura");
-var tdImc = paciente.querySelector(".info-imc");
-var altura = Number(tdAltura.textContent);
-var peso = Number(tdPeso.textContent);
-var imc = peso / (altura * altura);
-tdImc.textContent = imc;
-console.log(imc);
+var paciente = createPaciente(document.querySelector("#primeiro-paciente"));
+var mensagem = "";
+paciente.imc.textContent = calculaIMC(paciente.peso,paciente.altura);
+
+
+
+function calculaIMC(peso,altura){
+
+  if(validarCampos(peso,altura).length == 0){
+      return peso / (altura*altura);
+  }else{
+
+    return mensagem;
+  }
+}
+function createPaciente(paciente){
+  var novoPaciente = {peso:Number(paciente.querySelector(".info-peso").textContent)
+                     ,altura: Number(paciente.querySelector(".info-altura").textContent)
+                     ,imc:paciente.querySelector(".info-imc")}
+
+  return novoPaciente;
+}
+
+function validarCampos(peso,altura){
+  if (peso <= 0 || peso > 200){
+      mensagem = "peso invalido";
+      return mensagem;
+  }else if(altura >= 3 || altura <= 0){
+    mensagem = ' altura invalida ';
+    return mensagem;
+  }else{
+    return mensagem;
+  }
+
+
+}
