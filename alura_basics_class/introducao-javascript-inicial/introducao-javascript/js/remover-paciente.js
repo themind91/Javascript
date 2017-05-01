a33f4108da875f86727a15059bfb318129825ca1
Victor Rocha <themind1991@gmail.com>
2017-04-30 23:13:07 -0300

    renaming the principal file

--- alura_basics_class/introducao-javascript-inicial/introducao-javascript/js/remover-paciente.js
+++ alura_basics_class/introducao-javascript-inicial/introducao-javascript/js/remover-paciente.js
@@ -2,5 +2,12 @@ var tabela = document.querySelector("#tabela-pacientes");
 
 tabela.addEventListener("dblclick", function(event) {
   // acessando o pai de um elemento para remove-lo
+
+  if (event.target.tagName == 'TD') {
     event.target.parentNode.classList.add("fadeOut");
+    setTimeout(function() {
+         event.target.parentNode.remove();
+     }, 500);
+   }
+
 });
