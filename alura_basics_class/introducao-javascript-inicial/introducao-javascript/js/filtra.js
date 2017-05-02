5f4d14885e004ea15e48e67668c6a01c1bc0aecc
Victor Rocha <themind1991@gmail.com>
2017-05-02 20:19:28 -0300

    adicionando um arquivo para filtrar os usuarios

--- alura_basics_class/introducao-javascript-inicial/introducao-javascript/js/filtra.js
+++ alura_basics_class/introducao-javascript-inicial/introducao-javascript/js/filtra.js
@@ -6,5 +6,24 @@ campoFiltro.addEventListener("input", function() {
        var paciente = pacientes[i];
        var tdNome = paciente.querySelector(".info-nome");
        var nome = tdNome.textContent;
+
+       if (this.value.length > 0) {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+
+            if (nome != this.value) {
+                paciente.classList.add("invisivel");
+            } else {
+                paciente.classList.remove("invisivel");
+            }
+        }
+    }else {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
+    }    
    }
 });
