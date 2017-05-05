49a60d326cf48e6ffa4759d739cc3aef498aa23f
themind <themind1991@gmail.com>
2017-05-05 00:18:17 -0300

    updating code

--- javascript_advanced_2/javascript-avancado-i-projetoCompleto/aluraframe/client/js/app/models/ListaNegociacoes.js
+++ javascript_advanced_2/javascript-avancado-i-projetoCompleto/aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,18 +1,17 @@
 class ListaNegociacoes {
     
-    constructor(contexto,armadilha) {
+    constructor(armadilha) {
         
         this._negociacoes = [];
         this._armadilha = armadilha;
-        this._contexto = contexto;
     }
     
     adiciona(negociacao) {
         
         // utilizando a linda API de reflexao do JS
         this._negociacoes.push(negociacao);
-        //this._armadilha(this);
-        Reflect.apply(this._armadilha, this._contexto,[this]);
+        this._armadilha(this);
+        //Reflect.apply(this._armadilha, this._contexto,[this]);
     }
     
     get negociacoes() {
@@ -23,7 +22,7 @@ class ListaNegociacoes {
     esvazia(){
 
     	this._negociacoes = [];
-    	//this._armadilha(this);
-    	Reflect.apply(this._armadilha, this._contexto,[this]);
+    	this._armadilha(this);
+    	//Reflect.apply(this._armadilha, this._contexto,[this]);
     }
 }
