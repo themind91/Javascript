86c11e3933214b4bf88f8d13dbd8e9c7577ce437
vai <vitorstone@localhost.localdomain>
2017-12-26 06:07:41 -0200

    done

--- js_challenge/crawler/robot.js
+++ js_challenge/crawler/robot.js
@@ -1,11 +1,49 @@
 const Telegraf = require('telegraf');
 const TOKEN="473273944:AAFrJNZIrFyrpkrhmjM7m4OSeRvNOdYtLFY";
 const app = new Telegraf(TOKEN);
+const axios = require('axios');
 
 
-app.hears('hi', ctx =>{
-    return ctx.reply('Hello bb');
+app.on('text', ctx => {
 
-});
+    const subreddit = ctx.message.text;
 
+    if(subreddit.includes('/NadaPraFazer')){
+        var len = subreddit.lentgh;
+        var subreddits = subreddit.slice(13,len);
+        getSubReddits(subreddits.split(";"),ctx);
+        
+    }else{
+        ctx.reply('Digite o comando ex /NadaPraFazer cats;dogs;trips para obter resultados');
+    }
+  
+  });
+  
 app.startPolling();
+
+function getSubReddits(subreddits,ctx){
+      for(var i =0; i< subreddits.length; i++){
+        axios
+        .get(`https://reddit.com/r/${subreddits[i].trim()}.json`)
+        .then(res => {
+          const data = res.data.data;
+          if (data.children.length < 1)
+            return ctx.reply("The subreddit couldn't be found.");
+          else
+          replyAllData(data,ctx);
+        })
+    
+        .catch(err => console.log(err));
+      }
+     
+}
+
+function replyAllData(data,ctx){
+    for(var j =0; j < data.children.length; j++){
+        if(data.children[j].data.ups >= 5000){
+           var link = `https://reddit.com/${data.children[j].data.permalink}`;
+           ctx.reply(link);  
+        }
+    }
+
+}
